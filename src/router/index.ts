df96e1fabca764c6cd8e14b6b269394896e21ce2
Петр Халиков <pkhalikov@dataplus.ru>
2025-11-22 15:23:56 +0300

    fix(index.ts): изменил тип роутинга на хеш

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import MainPage from '@/views/MainPage.vue'
 import PatientPage from '@/views/PatientPage.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
