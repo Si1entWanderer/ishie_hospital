2ff3e69344e551ae16ae151e0ec148195d4e5ab5
Петр Халиков <p.xalikov@idaproject.com>
2025-11-28 19:40:22 +0300

    feat(patients): добавлен omdfr

--- src/assets/ts/patients/index.ts
+++ src/assets/ts/patients/index.ts
@@ -1,7 +1,15 @@
 import { patientAlyooha } from '@/assets/ts/patients/patientAlyooha'
 import { patientIisus } from '@/assets/ts/patients/patientIisus'
-import { patientPitaHavus } from '@/assets/ts/patients/patientPitaHavus'
 import { patientMisaki } from '@/assets/ts/patients/patientMisaki'
 import { patientNutvis } from '@/assets/ts/patients/patientNutvis'
+import { patientPitaHavus } from '@/assets/ts/patients/patientPitaHavus'
+import { patientOmdfr } from './patientOmdfr'
 
-export default [patientAlyooha, patientIisus, patientPitaHavus, patientMisaki, patientNutvis]
+export default [
+    patientAlyooha,
+    patientIisus,
+    patientPitaHavus,
+    patientMisaki,
+    patientNutvis,
+    patientOmdfr
+]
