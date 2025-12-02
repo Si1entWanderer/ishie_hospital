7eb1bbb2fe53e0a04faec33f940c2cde10a7ce9e
Петр Халиков <p.xalikov@idaproject.com>
2025-12-02 12:55:15 +0300

    feat(patients): добавил новый осмотр для omdfr

--- src/assets/ts/patients/patientOmdfr/index.ts
+++ src/assets/ts/patients/patientOmdfr/index.ts
@@ -3,13 +3,14 @@ import { ESex } from '@/assets/ts/enums'
 
 import omdfrAvatar from '@/assets/images/avatars/omdfrAvatar.jpg'
 import examination1 from './examinations/examination1'
+import examination2 from './examinations/examination2'
 
 export const patientOmdfr: TPatient = {
     id: '50822',
-    name: 'omdfr_j',
+    name: 'omdfr_died_j',
     image: omdfrAvatar,
     sex: ESex.male,
     birthDate: '2.03.2025',
     residentalAddress: 'Twitch канал Ishie_j',
-    examinations: [examination1],
+    examinations: [examination1, examination2],
 }
