8b89535494aaf5e29a3d0b78b9a71859e440cf08
Петр Халиков <p.xalikov@idaproject.com>
2025-12-03 16:15:26 +0300

    feat(patients): добавил Юмеко

--- src/assets/ts/patients/index.ts
+++ src/assets/ts/patients/index.ts
@@ -5,7 +5,7 @@ import { patientMisaki } from '@/assets/ts/patients/patientMisaki'
 import { patientNutvis } from '@/assets/ts/patients/patientNutvis'
 import { patientOmdfr } from '@/assets/ts/patients/patientOmdfr'
 import { patientPitaHavus } from '@/assets/ts/patients/patientPitaHavus'
-// import { patientYumekomoore } from '@/assets/ts/patients/patientYumekomoore'
+import { patientYumekomoore } from '@/assets/ts/patients/patientYumekomoore'
 
 export default [
     patientAlyooha,
@@ -15,5 +15,5 @@ export default [
     patientNutvis,
     patientOmdfr,
     patientMi5akiMei,
-    // patientYumekomoore
+    patientYumekomoore
 ]
