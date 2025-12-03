f54c3b80204f77f4b4f7812b94c556be2c107405
Петр Халиков <p.xalikov@idaproject.com>
2025-12-03 16:51:42 +0300

    feat(patients): добавил samr1na_

---
+++ src/assets/ts/patients/patientSamr1na/index.ts
@@ -0,0 +1,16 @@
+import type { TPatient } from '@/@types/patient'
+import { ESex, ETherapyMarksIds } from '@/assets/ts/enums'
+
+import placeholderAvatar from '@/assets/images/avatars/placeholderAvatar.jpg'
+import examination1 from './examinations/examination1'
+
+export const patientSamr1na: TPatient = {
+    id: '85771',
+    name: 'samr1na_',
+    image: placeholderAvatar,
+    sex: ESex.female,
+    birthDate: '13.07.2023',
+    residentalAddress: 'Twitch канал Ishie_j',
+    examinations: [examination1],
+    therapyMarks: [ETherapyMarksIds.pukaton2025],
+}
