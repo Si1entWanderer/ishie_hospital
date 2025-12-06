2bf3b466235b69952649d2583763ff265587a626
Петр Халиков <pkhalikov@dataplus.ru>
2025-12-06 20:06:22 +0300

    feat(patients): добавил аватар для Dreamdel

--- src/assets/ts/patients/patientDreamdel/index.ts
+++ src/assets/ts/patients/patientDreamdel/index.ts
@@ -1,13 +1,13 @@
 import type { TPatient } from '@/@types/patient'
 import { ESex, ETherapyMarksIds } from '@/assets/ts/enums'
 
-import placeholderAvatar from '@/assets/images/avatars/placeholderAvatar.jpg'
+import dreamdelAvatar from '@/assets/images/avatars/dreamdelAvatar.jpg'
 import examination1 from './examinations/examination1'
 
 export const patientDreamdel: TPatient = {
     id: '36316',
     name: 'Dreamdel_j',
-    image: placeholderAvatar,
+    image: dreamdelAvatar,
     sex: ESex.male,
     birthDate: '13.12.2024',
     residentalAddress: 'Twitch канал Ishie_j',
