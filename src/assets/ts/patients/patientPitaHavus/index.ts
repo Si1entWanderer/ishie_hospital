0af12a75843d10dd870c56ed05f50f3b276480e0
Петр Халиков <p.xalikov@idaproject.com>
2025-12-17 19:14:32 +0300

    feat(patients): добавил значок за клип Пите Хавусу

--- src/assets/ts/patients/patientPitaHavus/index.ts
+++ src/assets/ts/patients/patientPitaHavus/index.ts
@@ -2,9 +2,9 @@ import type { TPatient } from '@/@types/patient'
 import { ESex, ETherapyMarksIds } from '@/assets/ts/enums'
 
 import pitaHavusAvatar from '@/assets/images/avatars/pitaHavusAvatar.jpg'
+import { STAFF_NAMES } from '@/assets/ts/constants'
 import examination1 from './examinations/examination1'
 import examination2 from './examinations/examination2'
-import { STAFF_NAMES } from '@/assets/ts/constants'
 
 export const patientPitaHavus: TPatient = {
     id: '83578',
@@ -14,6 +14,6 @@ export const patientPitaHavus: TPatient = {
     birthDate: '30.03.2025',
     residentalAddress: 'Twitch канал Ishie_j',
     examinations: [examination1, examination2],
-    therapyMarks: [ETherapyMarksIds.pukaton2025],
+    therapyMarks: [ETherapyMarksIds.pukaton2025, ETherapyMarksIds.clip300],
     staffName: STAFF_NAMES.pitaHavus,
 }
