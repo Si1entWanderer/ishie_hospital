0de2178f6a56b7230ca1e44419276e100bf7a033
Петр Халиков <p.xalikov@idaproject.com>
2025-12-31 00:05:06 +0300

    feat(patients): добавил kunilika666

--- src/assets/ts/patients/index.ts
+++ src/assets/ts/patients/index.ts
@@ -1,5 +1,8 @@
 import { patientAlyooha } from '@/assets/ts/patients/patientAlyooha'
+import { patientDarkvels } from '@/assets/ts/patients/patientDarkvels'
+import { patientDreamdel } from '@/assets/ts/patients/patientDreamdel'
 import { patientIisus } from '@/assets/ts/patients/patientIisus'
+import { patientKunilika666 } from '@/assets/ts/patients/patientKunilika666'
 import { patientLis_7ete } from '@/assets/ts/patients/patientLis_7ete'
 import { patientMi5akiMei } from '@/assets/ts/patients/patientMi5akiMei'
 import { patientMisaki } from '@/assets/ts/patients/patientMisaki'
@@ -8,8 +11,6 @@ import { patientOmdfr } from '@/assets/ts/patients/patientOmdfr'
 import { patientPitaHavus } from '@/assets/ts/patients/patientPitaHavus'
 import { patientSamr1na } from '@/assets/ts/patients/patientSamr1na'
 import { patientYumekomoore } from '@/assets/ts/patients/patientYumekomoore'
-import { patientDreamdel } from '@/assets/ts/patients/patientDreamdel'
-import { patientDarkvels } from '@/assets/ts/patients/patientDarkvels'
 
 export default [
     patientAlyooha,
@@ -24,4 +25,5 @@ export default [
     patientLis_7ete,
     patientDreamdel,
     patientDarkvels,
+    patientKunilika666,
 ]
---
+++ src/assets/ts/patients/patientKunilika666/index.ts
@@ -0,0 +1,16 @@
+import type { TPatient } from '@/@types/patient'
+import { ESex, ETherapyMarksIds } from '@/assets/ts/enums'
+
+import placeholderAvatar from '@/assets/images/avatars/placeholderAvatar.jpg'
+import examination1 from './examinations/examination1'
+
+export const patientKunilika666: TPatient = {
+    id: '82762',
+    name: 'kunilika666',
+    image: placeholderAvatar,
+    sex: ESex.female,
+    birthDate: '21.06.2025',
+    residentalAddress: 'Twitch канал Ishie_j',
+    examinations: [examination1],
+    therapyMarks: [ETherapyMarksIds.roulette],
+}
