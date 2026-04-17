8ed61e33fc74b51f43b76a1589a18f0f01997946
Петр Халиков <p.xalikov@idaproject.com>
2026-04-17 03:27:41 +0300

    fix(patients): исправил карточку falaqq_j

--- src/assets/ts/patients/patientFalaqq_j/index.ts
+++ src/assets/ts/patients/patientFalaqq_j/index.ts
@@ -1,12 +1,14 @@
 import type { IPatient } from '@/@types/patient'
 import { ESex, ETherapyMarksIds } from '@/assets/ts/enums'
 
+import falaqq_jAvatar from '@/assets/images/avatars/falaqq_jAvatar.jpg'
+
 import examination1 from './examinations/examination1'
 
 const patient: IPatient = {
     id: '77154',
     name: 'falaqq_j',
-    image: null,
+    image: falaqq_jAvatar,
     sex: ESex.male,
     birthDate: '16.04.2025',
     residentalAddress: 'Twitch канал Ishie_j',
