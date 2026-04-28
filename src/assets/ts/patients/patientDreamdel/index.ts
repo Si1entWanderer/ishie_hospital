b27939c7c1fcc80931ca8b769cc49acc840c380c
Петр Халиков <p.xalikov@idaproject.com>
2026-04-28 15:59:28 +0300

    feat(patients): добавил обследование для Dreamdel

--- src/assets/ts/patients/patientDreamdel/index.ts
+++ src/assets/ts/patients/patientDreamdel/index.ts
@@ -3,6 +3,7 @@ import { ESex, ETherapyMarksIds } from '@/assets/ts/enums'
 
 import dreamdelAvatar from '@/assets/images/avatars/dreamdelAvatar.jpg'
 import examination1 from './examinations/examination1'
+import examination2 from './examinations/examination2'
 
 const patientDreamdel: IPatient = {
     id: '36316',
@@ -11,8 +12,8 @@ const patientDreamdel: IPatient = {
     sex: ESex.male,
     birthDate: '13.12.2024',
     residentalAddress: 'Twitch канал Ishie_j',
-    examinations: [examination1],
-    therapyMarks: [ETherapyMarksIds.pukaton2025],
+    examinations: [examination1, examination2],
+    therapyMarks: [ETherapyMarksIds.pukaton2025, ETherapyMarksIds.streak100],
     registrationDate: 1764979200000,
 }
 
